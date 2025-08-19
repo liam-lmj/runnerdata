e07d1c8920fd07436f4e17b7b26e61b8d4e9d136
liam-lmj <liammatthewjameswhite@gmail.com>
2025-08-19 22:25:28 +0100

    added basic functionality to app and created some inital html files

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dashboard import init_dashboard
 
 app = Flask(__name__)
 
 init_dashboard(app)
 
+#test data
+#TODO pull data from database
+#TODO styling for table etc
+
+WEEKLY_MILEAGE = [
+    {"week": "week1", "Monday": 10, "Tuesday": 10, "Wednesday": 10, "Thursday": 10,
+     "Friday": 12, "Saturday": 12, "Sunday": 14, "total_mileage": 80},
+    {"week": "week2", "Monday": 10, "Tuesday": 10, "Wednesday": 10, "Thursday": 10,
+     "Friday": 12, "Saturday": 12, "Sunday": 14, "total_mileage": 80}
+]
+
 @app.route("/")
-def home():
-    return "<h1>Welcome to the running data app</h1><p>Go to: <a href='/dash/'>Runner Data!</a></p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/mileage")
+def mileage():
+    return render_template("mileagechart.html", weekly_mileage=WEEKLY_MILEAGE)
 
 if __name__ == "__main__":
     app.run(debug=True)
